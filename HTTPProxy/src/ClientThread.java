febb51933402688e13d74f924760ba3b4a08f8c5
shent3 <St123698*>
2017-02-12 18:29:41 -0800

    http proxy done

---
+++ HTTPProxy/src/ClientThread.java
@@ -0,0 +1,9 @@
+/**
+ * Created by tongshen on 2/12/17.
+ */
+public class ClientThread extends Thread {
+    @Override
+    public void run() {
+
+    }
+}
--- HTTPProxy/src/HTTPProxy.java
+++ HTTPProxy/src/HTTPProxy.java
@@ -1,5 +1,43 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by tongshen on 2/12/17.
  */
+
 public class HTTPProxy {
+    public static int portNumber;
+
+    public static void main() {
+        Scanner scan = new Scanner(System.in);
+        runProxy(scan);
+    }
+
+    public static void runProxy(Scanner scan) {
+        while (true) {
+            if (scan.hasNextInt()) {
+                portNumber = scan.nextInt();
+            } else {
+                System.out.println("Invalid port number.");
+            }
+
+            if (portNumber < 0 || portNumber > 65536) {
+                System.out.println("Invalid port number.");
+            } else {
+                break;
+            }
+        }
+
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(portNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Thread thread = new ClientThread();
+        thread.run();
+
+    }
 }
